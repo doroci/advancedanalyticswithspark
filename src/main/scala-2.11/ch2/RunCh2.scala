56f054eb9a8d8f8ca5d2d23772bdf7031ac5dc99
doroci <hiiln0273@gmail.com>
2017-02-05 22:22:33 +0900

    standAlone Cluster Test

--- src/main/scala-2.11/ch2/RunCh2.scala
+++ src/main/scala-2.11/ch2/RunCh2.scala
@@ -1,7 +1,7 @@
 package ch2
 
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import org.apache.spark.sql.functions._ // for lit(), first(), etc.
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession} // for lit(), first(), etc.
 
 
 
@@ -24,75 +24,78 @@ case class MatchData
   is_match: Boolean
 )
 
-object RunCh2 extends App {
-  val spark = SparkSession.builder
-    .appName("Intro").master("local[4]")
-    .getOrCreate
-  import spark.implicits._
+object RunCh2 {
+  def main(args: Array[String]) {
+
+    val spark = SparkSession.builder
+      .appName("Intro").master("local[4]")
+      .getOrCreate
+    import spark.implicits._
 
 
 
-  // ex) Users/lee/Documents/sparks/Season2/Advanced_Analytics_With_Spark/linkage/block_1.csv
-  val preview = spark.read.csv("/Users/lee/Documents/sparks/Season2/Advanced_Analytics_With_Spark/linkage/block_1.csv")
-  preview.show()
-  preview.schema.foreach(println)
+    // ex) Users/lee/Documents/sparks/Season2/Advanced_Analytics_With_Spark/linkage/block_1.csv
+    val preview = spark.read.csv("/Users/lee/Documents/sparks/Season2/Advanced_Analytics_With_Spark/linkage/block_1.csv")
+    preview.show()
+    preview.schema.foreach(println)
 
-  val parsed = spark.read
-    .option("header", "true") // 파일의 첫 줄을 필드 명으로 사용
-    .option("nullValue", "?") // 필드 데이터를 변경( "?" => null )
-    .option("inferSchema", "true") // 데이터 타입을 추론한다.
-    .csv("/Users/lee/Documents/sparks/Season2/Advanced_Analytics_With_Spark/linkage/block_1.csv")
+    val parsed = spark.read
+      .option("header", "true") // 파일의 첫 줄을 필드 명으로 사용
+      .option("nullValue", "?") // 필드 데이터를 변경( "?" => null )
+      .option("inferSchema", "true") // 데이터 타입을 추론한다.
+      .csv("/Users/lee/Documents/sparks/Season2/Advanced_Analytics_With_Spark/linkage/block_1.csv")
 
-  parsed.show()
-  val schema = parsed.schema
-  schema.foreach(println)
+    parsed.show()
+    val schema = parsed.schema
+    schema.foreach(println)
 
-  parsed.count()
-  parsed.cache()
-  parsed.groupBy("is_match").count().orderBy($"count".desc).show()
+    parsed.count()
+    parsed.cache()
+    parsed.groupBy("is_match").count().orderBy($"count".desc).show()
 
-  parsed.createOrReplaceTempView("parks") // createOrReplaceTempView() : 사용중인 DataFrame의 하나의 뷰를 생성한다.
-  spark.sql(
-    """
+    parsed.createOrReplaceTempView("parks") // createOrReplaceTempView() : 사용중인 DataFrame의 하나의 뷰를 생성한다.
+    spark.sql(
+      """
       SELECT is_match, COUNT(*) cnt
       FROM parks
       GROUP BY is_match
       ORDER BY cnt DESC
-    """).show()
+      """).show()
 
-  val summary = parsed.describe() // describe() : numeric columns, including count, mean, stddev, min, and max의 통계를 리턴해준다.
-  // ex)  parsed.describe("id_1","id_2") : 인자값을 넣어 해당 필드만 적용할 수 있다.
+    val summary = parsed.describe() // describe() : numeric columns, including count, mean, stddev, min, and max의 통계를 리턴해준다.
+    // ex)  parsed.describe("id_1","id_2") : 인자값을 넣어 해당 필드만 적용할 수 있다.
 
-  summary.show()
+    summary.show()
 
-  summary
-    .select("summary", "cmp_fname_c1", "cmp_fname_c2")
-    .show()
+    summary
+      .select("summary", "cmp_fname_c1", "cmp_fname_c2")
+      .show()
 
-  val matches = parsed.where("is_match = true")
-  val misses = parsed.filter($"is_match" === lit(false))
-  val matchSummary = matches.describe()
-  val missSummary = misses.describe()
+    val matches = parsed.where("is_match = true")
+    val misses = parsed.filter($"is_match" === lit(false))
+    val matchSummary = matches.describe()
+    val missSummary = misses.describe()
 
-  val matchSummaryT = pivotSummary(matchSummary)
-  val missSummaryT = pivotSummary(missSummary)
-  matchSummaryT.createOrReplaceTempView("match_desc")
-  missSummaryT.createOrReplaceTempView("miss_desc")
-  spark.sql(
-    """
+    val matchSummaryT = pivotSummary(matchSummary)
+    val missSummaryT = pivotSummary(missSummary)
+    matchSummaryT.createOrReplaceTempView("match_desc")
+    missSummaryT.createOrReplaceTempView("miss_desc")
+    spark.sql(
+      """
       SELECT a.field, a.count + b.count total, a.mean - b.mean delta
       FROM match_desc a INNER JOIN miss_desc b ON a.field = b.field
       ORDER BY delta DESC, total DESC
-    """).show()
+      """).show()
 
-  val matchData = parsed.as[
-    MatchData] // as() : 새로운 DataSet를 리턴한다.
-  val scored = matchData.map(md => {
-      (scoreMatchData(md), md.
-        is_match)
-    }).toDF("score", "is_match")
-  crossTabs(scored, 4.0).show()
+    val matchData = parsed.as[
+      MatchData] // as() : 새로운 DataSet를 리턴한다.
+    val scored = matchData.map(md => {
+        (scoreMatchData(md), md.
+          is_match)
+      }).toDF("score", "is_match")
+    crossTabs(scored, 4.0).show()
 
+  }
 
   def crossTabs(scored: DataFrame, t: Double): DataFrame = {
     scored.
@@ -121,8 +124,7 @@ object RunCh2 extends App {
   }
 
   def longForm(desc: DataFrame): DataFrame = {
-    import desc.
-    sparkSession.implicits._ // DataFrame으로 변환을 위한 도움을 준다.
+    import desc.sparkSession.implicits._ // DataFrame으로 변환을 위한 도움을 준다.
     val schema = desc.schema
     desc.flatMap(row => {
       val metric = row.getString(0)
---
+++ src/main/scala-2.11/ch2/StandAloneTest.scala
@@ -0,0 +1,54 @@
+package ch2
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by lee on 2017. 2. 5..
+  */
+object StandAloneTest {
+
+
+  def main(args: Array[String]) {
+
+    //스파크 세션 설정
+    val spark = SparkSession.builder
+      .appName("standAlone-cluster-test").master("local[6]")
+      .getOrCreate
+    import spark.implicits._
+
+    // csv파일 읽기
+    val preview = spark
+      .read
+      .csv("/Users/lee/Downloads/ml-latest/ratings.csv")
+    preview.show
+
+    // option값 설정
+    val parsed = spark.read
+      .option("header", "true") // 파일의 첫 줄을 필드 명으로 사용
+      .option("nullValue", "?") // 필드 데이터를 변경( "?" => null )
+      .option("inferSchema", "true") // 데이터 타입을 추론한다.
+      .csv("/Users/lee/Downloads/ml-latest/ratings.csv")
+    parsed.show
+
+    parsed.count
+    parsed.cache
+    parsed
+      .groupBy("movieId")
+      .count
+      .orderBy($"count".desc)
+      .show
+
+    // createOrReplaceTempView(): 사용중인 DataFrame의 하나의 뷰를 생성한다.
+    val createView = parsed.createOrReplaceTempView("parks")
+    spark.sql("""
+  SELECT movieId, COUNT(*) cnt
+  FROM parks
+  GROUP BY movieId
+  ORDER BY cnt DESC
+              """).show
+
+    // describe() : numeric columns, including count, mean, stddev, min, and max의 통계를 리턴해준다.
+    val summary = parsed.describe()
+    summary.show
+  }
+}
